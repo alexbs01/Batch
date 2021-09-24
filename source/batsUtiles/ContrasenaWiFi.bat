0841d8d0fafcb8acf658e16d2115755ce8eb0058
alexbs01 <alex.becerra.suarez01@gmail.com>
2021-09-24 08:51:03 +0200

    ContrasenaWifi actualizado para solo mostrar la contraseña de la red

--- source/batsUtiles/ContrasenaWiFi.bat
+++ source/batsUtiles/ContrasenaWiFi.bat
@@ -5,5 +5,5 @@ netsh wlan show profile
 
 SET /P networkname=Nombre de la red: 
 
-netsh wlan show profile %networkname% key=clear
+netsh wlan show profile %networkname% key=clear | find "Contenido de la clave"
 PAUSE
